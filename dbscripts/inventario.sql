77dbc6b78342aa19a452f3b4b462a193167ba490
rockdrigo <correo.rodrigo@gmail.com>
2022-06-23 10:49:59 -0500

    design db progress

--- dbscripts/inventario.sql
+++ dbscripts/inventario.sql
@@ -1,23 +1,21 @@
 BEGIN TRANSACTION;
 CREATE TABLE IF NOT EXISTS "Categorias" (
        "Id"  	    	  TEXT NOT NULL UNIQUE,
-       "Item"		  INTEGER AUTOINCREMENT,
-       "Nombre"	          TEXT NOT NULL
+       "Nombre"	          TEXT NOT NULL,
        PRIMARY KEY("Id")
 );
 
 -- Init data
-INSERT INTO Categorias  (Id, Nombre) VALUES ('3f75daaa-04c6-4d04-aaa1-e243a00ef2c6', 'General');
-INSERT INTO Categorias  (Id, Nombre) VALUES ('e6e15392-7c43-49a6-bb5c-4ed6a6794c62', 'SHEIN');
-INSERT INTO Categorias  (Id, Nombre) VALUES ('84debfc5-a23b-44c6-9945-248dfc4e4239', 'Monografias');
-INSERT INTO Categorias  (Id, Nombre) VALUES ('20e61d99-6d05-4b8b-a9b3-bf5273558f9d', 'Biografias');
-INSERT INTO Categorias  (Id, Nombre) VALUES ('c046b206-5a37-4463-925c-c195e267cc74', 'Kawaii');
+INSERT INTO Categorias (id, Nombre) VALUES ('3f75daaa-04c6-4d04-aaa1-e243a00ef2c6', 'General');
+INSERT INTO Categorias (id, Nombre) VALUES ('e6e15392-7c43-49a6-bb5c-4ed6a6794c62', 'SHEIN');
+INSERT INTO Categorias (id, Nombre) VALUES ('84debfc5-a23b-44c6-9945-248dfc4e4239', 'Monografias');
+INSERT INTO Categorias (id, Nombre) VALUES ('20e61d99-6d05-4b8b-a9b3-bf5273558f9d', 'Biografias');
+INSERT INTO Categorias (id, Nombre) VALUES ('c046b206-5a37-4463-925c-c195e267cc74', 'Kawaii');
 
 
 CREATE TABLE IF NOT EXISTS "UnidadesMedida" (
        "Id"  	    	  TEXT NOT NULL UNIQUE,
-       "Item"		  INTEGER AUTOINCREMENT,
-       "Nombre"	          TEXT NOT NULL
+       "Nombre"	          TEXT NOT NULL,
        PRIMARY KEY("Id")
 );
 
@@ -29,7 +27,6 @@ INSERT INTO Categorias  (Id, Nombre) VALUES ('710ad3c8-b75e-49b3-a3cd-43507ac959
 
 CREATE TABLE IF NOT EXISTS "Productos" (
        "Id"  	    	   	       TEXT NOT NULL UNIQUE,
-       "Item"		  	       INTEGER AUTOINCREMENT,
        "Nombre"	               	       TEXT NOT NULL,
        "UnidadMedidaId"	       	       TEXT NOT NULL,
        "CodigoBarrasItem"	       TEXT NOT NULL, -- Si el item tiene codigo de barras por ejemplo codigo barras en un lapiz
@@ -48,21 +45,19 @@ CREATE TABLE IF NOT EXISTS "CategoriasProductos" (
 
 CREATE TABLE IF NOT EXISTS "Compras" (
        "Id"  	    	  TEXT NOT NULL UNIQUE,
-       "Item"	          INTEGER AUTOINCREMENT,
        "Notas"	  	  TEXT,
        "FechaCompra"      TEXT NOT NULL,
        "FechaArribo"	  TEXT,
-       "CostoPaqueteria"  NUMERIC -- decimal
-       "TotalCompra"      NUMERIC -- decimal
+       "CostoPaqueteria"  NUMERIC, -- decimal
+       "TotalCompra"      NUMERIC, -- decimal
        PRIMARY KEY("Id")
 );
 
 CREATE TABLE IF NOT EXISTS "ComprasProductos" (
        "Id"  	    	  TEXT NOT NULL UNIQUE,
-       "Item"	          INTEGER AUTOINCREMENT,
        "ProductoId"	  TEXT NOT NULL,
        "CompraId"	  TEXT NOT NULL,
-       "Cantidad"         NUMERIC -- decimal
+       "Cantidad"         NUMERIC, -- decimal
        PRIMARY KEY("Id"),
        FOREIGN KEY("ProductoId") REFERENCES "Productos"("Id") ON DELETE CASCADE
        FOREIGN KEY("CompraId") REFERENCES "Compras"("Id") ON DELETE CASCADE
@@ -71,7 +66,6 @@ CREATE TABLE IF NOT EXISTS "ComprasProductos" (
 
 CREATE TABLE IF NOT EXISTS "Ventas" (
        "Id"  	    	  TEXT NOT NULL UNIQUE,
-       "Item"	          INTEGER AUTOINCREMENT,
        "Notas"	  	  TEXT,
        "FechaVenta"       TEXT NOT NULL,
        PRIMARY KEY("Id")
@@ -79,10 +73,9 @@ CREATE TABLE IF NOT EXISTS "Ventas" (
 
 CREATE TABLE IF NOT EXISTS "VentasProductos" (
        "Id"  	    	  TEXT NOT NULL UNIQUE,
-       "Item"	          INTEGER AUTOINCREMENT,
        "ProductoId"	  TEXT NOT NULL,
        "VentaId"	  TEXT NOT NULL,
-       "Cantidad"         NUMERIC -- decimal
+       "Cantidad"         NUMERIC, -- decimal
        PRIMARY KEY("Id"),
        FOREIGN KEY("ProductoId") REFERENCES "Productos"("Id") ON DELETE CASCADE
        FOREIGN KEY("VentaId") REFERENCES "Ventas"("Id") ON DELETE CASCADE
