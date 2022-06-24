7f04513ea1c687eff772698ddb2da4941667f686
rockdrigo <correo.rodrigo@gmail.com>
2022-06-24 15:21:25 -0500

    compras repo

---
+++ Ro.Inventario.Web/Entities/Compra.cs
@@ -0,0 +1,19 @@
+namespace Ro.Inventario.Web.Entities;
+
+public class Compra
+{    
+    public Compra()
+    {
+        Id = Guid.NewGuid();
+        Notas = string.Empty;
+        FechaCreado = DateTime.Now;
+        FechaFactura = DateTime.Now;
+        
+    }
+    public Guid Id { get; set; }
+    public string Notas { get; set; }
+    public DateTime FechaFactura { get; set; }
+    public DateTime FechaCreado { get; set; }
+    public decimal CostoPaqueteria { get; set; }
+    public decimal TotalFactura { get; set; }
+}
---
+++ Ro.Inventario.Web/Repos/ComprasRepo.cs
@@ -0,0 +1,61 @@
+using Ro.SQLite.Data;
+using Ro.Inventario.Web.Entities;
+using System.Data;
+
+namespace Ro.Inventario.Web.Repos;
+
+public interface IComprasRepo
+{
+    Task<int> Save(Compra it);
+    Task<Compra> GetOne(Guid id);
+}
+
+public class ComprasRepo : IComprasRepo
+{
+    private const string DATE_FORMAT = "yyy-MM-dd HH:mm:ss.fff";
+    private IDbAsync Db;
+    public ComprasRepo(IDbAsync db)
+    {
+        this.Db = db;
+    }
+    
+    public Task<int> Save(Compra it)
+    {
+        var sql = @"INSERT INTO Compras (Id,Notas,FechaFactura,FechaCreado,CostoPaqueteria,TotalFactura) VALUES 
+                    (@id,@notas,@fechaFactura,@fechaCreado,@costoPaqueteria,@totalFactura);";
+        var cmd = sql.ToCmd
+        (
+            "@id".ToParam(DbType.String, it.Id.ToString()),
+            "@notas".ToParam(DbType.String, it.Notas.ToString()),
+            "@fechaFactura".ToParam(DbType.String, it.FechaFactura.ToString(DATE_FORMAT)),
+            "@fechaCreado".ToParam(DbType.String, it.FechaCreado.ToString(DATE_FORMAT)),
+            "@costoPaqueteria".ToParam(DbType.Decimal, it.CostoPaqueteria),
+            "@totalFactura".ToParam(DbType.Decimal, it.TotalFactura)
+        );
+        return Db.ExecuteNonQuery(cmd);
+    }
+
+    public Task<Compra> GetOne(Guid id)
+    {
+        var sql = "SELECT Id,Notas,FechaFactura,FechaCreado,CostoPaqueteria,TotalFactura FROM Compras WHERE Id = @id";
+        var cmd = sql.ToCmd
+        (            
+            "@id".ToParam(DbType.String, id.ToString())
+        );
+        return Db.GetOneRow(cmd, GetData);
+    }
+   
+
+    private Compra GetData(IDataReader dr)
+    {
+        return new Compra()
+        {
+            Id = Guid.Parse(dr.GetString("Id")),
+            Notas = dr.GetString("Notas"),
+            FechaFactura = DateTime.Parse(dr.GetString("FechaFactura")),
+            FechaCreado = DateTime.Parse(dr.GetString("FechaCreado")),
+            CostoPaqueteria = dr.GetDecilmal("CostoPaqueteria"),
+            TotalFactura = dr.GetDecilmal("TotalFactura")
+        };
+    }   
+}
