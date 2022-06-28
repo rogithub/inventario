efb45c75872bcfd456287165223009830c03c926
rockdrigo <correo.rodrigo@gmail.com>
2022-06-28 16:54:11 -0500

    full search text

--- Ro.Inventario.Web/Controllers/VentasController.cs
+++ Ro.Inventario.Web/Controllers/VentasController.cs
@@ -1,16 +1,23 @@
 using System.Diagnostics;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Ro.Inventario.Web.Models;
+using Ro.Inventario.Web.Repos;
 
 namespace Ro.Inventario.Web.Controllers;
 
 public class VentasController : Controller
 {
+    private readonly IProductosRepo _productos;
     private readonly ILogger<VentasController> _logger;
 
-    public VentasController(ILogger<VentasController> logger)
+    public VentasController(
+        ILogger<VentasController> logger,
+        IProductosRepo productos
+        )
     {
         _logger = logger;
+        _productos = productos;
     }
 
     public IActionResult Index()
@@ -21,6 +28,17 @@ public class VentasController : Controller
     public IActionResult Nuevo()
     {
         return View();
+    }
+    
+    public async Task<IActionResult> BuscarProducto(string pattern)
+    {
+        var prods = await _productos.FulSearchText(pattern);
+        var res = (
+            from p in prods select 
+            new { id = p.Id, value = p.Nombre }
+        ).ToArray();
+
+        return Json(res, true);
     }
 
 
