d556837eb44b0f1be820131497121afce3295ec4
rodrigo debian <correo.rodrigo@gmail.com>
2022-07-03 08:06:49 -0500

    get by text

--- Ro.Inventario.Web/Repos/UnidadMedidaRepo.cs
+++ Ro.Inventario.Web/Repos/UnidadMedidaRepo.cs
@@ -8,11 +8,11 @@ namespace Ro.Inventario.Web.Repos;
 
 public interface IUnidadMedidaRepo
 {
-    Task<int> Save(UnidadMedida it);
+    Task<UnidadMedida> GetOne(string nombre);
     Task<UnidadMedida> GetOne(Guid id);
     Task<int> BulkSave(string[] list);
     Task<IEnumerable<UnidadMedida>> GetAll();
-    Task<Dictionary<string,Guid>> GetDictionary();
+    Task<Dictionary<string, Guid>> GetDictionary();
 }
 
 public class UnidadMedidaRepo : IUnidadMedidaRepo
@@ -23,23 +23,22 @@ public class UnidadMedidaRepo : IUnidadMedidaRepo
     {
         this.Db = db;
     }
-    
-    public Task<int> Save(UnidadMedida it)
+
+    public Task<UnidadMedida> GetOne(string nombre)
     {
-        var sql = "INSERT INTO UnidadesMedida (ID, NOMBRE) VALUES (@id, @nombre)";
+        var sql = "SELECT ID, NOMBRE FROM UnidadesMedida WHERE NOMBRE = @nombre";
         var cmd = sql.ToCmd
         (
-            "@id".ToParam(DbType.String, it.Id.ToString()),
-            "@nombre".ToParam(DbType.String, it.Id.ToString())
+            "@nombre".ToParam(DbType.String, nombre.ToString())
         );
-        return Db.ExecuteNonQuery(cmd);
+        return Db.GetOneRow(cmd, GetData);
     }
 
     public Task<UnidadMedida> GetOne(Guid id)
     {
         var sql = "SELECT ID, NOMBRE FROM UnidadesMedida WHERE Id = @id";
         var cmd = sql.ToCmd
-        (            
+        (
             "@id".ToParam(DbType.String, id.ToString())
         );
         return Db.GetOneRow(cmd, GetData);
@@ -77,9 +76,9 @@ public class UnidadMedidaRepo : IUnidadMedidaRepo
         return Db.GetRows(cmd, GetData);
     }
 
-    public async Task<Dictionary<string,Guid>> GetDictionary()
+    public async Task<Dictionary<string, Guid>> GetDictionary()
     {
-        var d = new Dictionary<string,Guid>();
+        var d = new Dictionary<string, Guid>();
 
         var all = await GetAll();
         foreach (var it in all)
@@ -89,7 +88,7 @@ public class UnidadMedidaRepo : IUnidadMedidaRepo
                 d.Add(it.Nombre, it.Id);
             }
         }
-        
+
         return d;
     }
 }
