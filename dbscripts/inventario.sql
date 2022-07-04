5fd65a5b7892c7f9e898556cb9a137b027b85352
rockdrigo <correo.rodrigo@gmail.com>
2022-07-04 16:23:37 -0500

    v_productos

--- dbscripts/inventario.sql
+++ dbscripts/inventario.sql
@@ -106,36 +106,3 @@ CREATE TABLE IF NOT EXISTS "AjustesProductos" (
        FOREIGN KEY("AjusteId") REFERENCES "Ajustes"("Id") ON DELETE CASCADE
 
 );
-
--- Vista Productos
-DROP VIEW IF EXISTS v_productos;
-CREATE VIEW v_productos
-AS
-SELECT 
-	p.nid, p.Id, p.Nombre, c.Nombre as Categoria, um.Nombre as UnidadMedida,
-	(SELECT PrecioVenta FROM PreciosProductos pp WHERE p.Id = pp.ProductoId ORDER BY datetime(pp.FechaCreado) DESC LIMIT 1) as PrecioVenta,
-       (SELECT PrecioCompra FROM Compras c JOIN ComprasProductos cp ON c.Id = cp.CompraId WHERE cp.ProductoId = p.Id ORDER BY datetime(c.FechaCreado) DESC LIMIT 1) as PrecioCompra,
-	p.CodigoBarrasItem,
-	p.CodigoBarrasCaja
-
-FROM Productos p 
-JOIN CategoriasProductos cp ON p.Id = cp.ProductoId
-JOIN Categorias c ON c.Id = cp.CategoriaId
-JOIN UnidadesMedida um ON p.UnidadMedidaId = um.Id;
-
--- Vista Inventario
-DROP VIEW IF EXISTS v_inventario;
-CREATE VIEW v_inventario
-AS
-SELECT 
-	p.*,	
-	((SELECT ifnull(SUM(cp.Cantidad), 0) FROM ComprasProductos cp WHERE cp.ProductoId = p.Id) 
-	- (SELECT ifnull(SUM(ap.Cantidad), 0) FROM AjustesProductos ap WHERE ap.ProductoId = p.Id)) AS Stock
-FROM 
-	v_productos p;
-	
-COMMIT;
-
-
-
-
--- dbscripts/reportes.sql
+++ dbscripts/reportes.sql
@@ -1,5 +1,34 @@
 BEGIN TRANSACTION;
 
+
+-- Vista Productos
+DROP VIEW IF EXISTS v_productos;
+CREATE VIEW v_productos
+AS
+SELECT 
+	p.nid, p.Id, p.Nombre, c.Nombre as Categoria, um.Nombre as UnidadMedida,
+	(SELECT PrecioVenta FROM PreciosProductos pp WHERE p.Id = pp.ProductoId ORDER BY datetime(pp.FechaCreado) DESC LIMIT 1) as PrecioVenta,
+       (SELECT PrecioCompra FROM Compras c JOIN ComprasProductos cp ON c.Id = cp.CompraId WHERE cp.ProductoId = p.Id ORDER BY datetime(c.FechaCreado) DESC LIMIT 1) as PrecioCompra,
+	p.CodigoBarrasItem,
+	p.CodigoBarrasCaja
+
+FROM Productos p 
+JOIN CategoriasProductos cp ON p.Id = cp.ProductoId
+JOIN Categorias c ON c.Id = cp.CategoriaId
+JOIN UnidadesMedida um ON p.UnidadMedidaId = um.Id;
+
+-- Vista Inventario
+DROP VIEW IF EXISTS v_inventario;
+CREATE VIEW v_inventario
+AS
+SELECT 
+	p.*,	
+	((SELECT ifnull(SUM(cp.Cantidad), 0) FROM ComprasProductos cp WHERE cp.ProductoId = p.Id) 
+	- (SELECT ifnull(SUM(ap.Cantidad), 0) FROM AjustesProductos ap WHERE ap.ProductoId = p.Id)) AS Stock
+FROM 
+	v_productos p;
+    	
+
 -- Reporte de Ventas
 DROP VIEW IF EXISTS rpt_ventas;
 CREATE VIEW rpt_ventas
