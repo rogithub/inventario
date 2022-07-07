9fe0954532688ce24a7f7a68fccb4b71065ad9d7
rodrigo debian <correo.rodrigo@gmail.com>
2022-07-07 07:21:09 -0500

    comments

--- dbscripts/reportes.sql
+++ dbscripts/reportes.sql
@@ -28,7 +28,7 @@ FROM
 	v_productos p;
 
 
--- REVISAR NO REGRESA TODOS LOS PRODUCTOS (solo los que se han vendido)
+-- REVISAR NO REGRESA TODOS LOS PRODUCTOS (solo los que se han vendido count debe ser ap count)
 DROP VIEW IF EXISTS v_ventas_productos;
 CREATE VIEW v_ventas_productos
 AS
