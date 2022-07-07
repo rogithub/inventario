6a5b2debb4e790cf7f9ac05e175d7128b026eccc
rockdrigo <correo.rodrigo@gmail.com>
2022-07-07 08:57:31 -0500

    pruebas reporte de ventas

--- dbscripts/reportes.sql
+++ dbscripts/reportes.sql
@@ -28,7 +28,7 @@ FROM
 	v_productos p;
 
 
--- REVISAR NO REGRESA TODOS LOS PRODUCTOS (solo los que se han vendido count debe ser ap count)
+-- REVISAR (count debe ser AjustesProductos count)
 DROP VIEW IF EXISTS v_ventas_productos;
 CREATE VIEW v_ventas_productos
 AS
@@ -36,28 +36,53 @@ SELECT
 	ap.Id as AjusteProductoId,
 	ap.ProductoId,
 	ap.AjusteId,
-	ap.Cantidad
-	--, cast(pp.PrecioVenta AS FLOAT) as UltimoPrecioVenta
+	datetime( a.FechaAjuste ) as FechaAjuste,
+	cast(a.Pago AS FLOAT) as Pago,
+	cast(a.Cambio AS FLOAT) as Cambio,
+	cast(ap.Cantidad AS FLOAT) as Cantidad
+	, cast(pp.PrecioVenta AS FLOAT) as UltimoPrecioVenta
 	, cast(cp.PrecioCompra AS FLOAT) as UltimoPrecioCompra	
 FROM 
 	AjustesProductos ap JOIN Ajustes a ON a.Id = ap.AjusteId JOIN productos p on ap.ProductoId = p.id
-	--JOIN PreciosProductos pp ON pp.Id = (SELECT Id FROM PreciosProductos WHERE  datetime( FechaCreado ) <= datetime( a.FechaAjuste ) AND ProductoId = p.Id ORDER BY datetime(FechaCreado) DESC LIMIT 1)
-	JOIN ComprasProductos cp ON cp.Id = (SELECT cp.Id FROM ComprasProductos cp JOIN Compras c on cp.CompraId = c.Id WHERE datetime( c.FechaFactura ) <= datetime( a.FechaAjuste ) AND cp.ProductoId = p.Id ORDER BY datetime(c.FechaFactura) DESC LIMIT 1)
-WHERE TipoAjuste = 0  
+	JOIN PreciosProductos pp ON pp.Id = 
+		(
+			SELECT Id FROM PreciosProductos 
+			WHERE 
+				ProductoId = p.Id
+				AND  (datetime(FechaCreado) <= datetime( a.FechaAjuste ) OR (SELECT COUNT(*) FROM PreciosProductos WHERE ProductoId = p.Id) = 1)
+			ORDER BY datetime(FechaCreado) DESC LIMIT 1
+		)
+	JOIN ComprasProductos cp ON cp.Id = 
+		(
+			SELECT cp.Id FROM ComprasProductos cp JOIN Compras c on cp.CompraId = c.Id 
+			WHERE 
+				cp.ProductoId = p.Id
+				AND  datetime(c.FechaFactura) <= datetime(a.FechaAjuste)
+			ORDER BY datetime(c.FechaFactura) DESC LIMIT 1)
+WHERE TipoAjuste = 0;
 
--- REVISAR PRECIOS
---SELECT 
---	a.Id as AjusteId,
---	p.Id as ProductoId,
---	p.Nombre,	
---	a.FechaAjuste, 
---	CAST(ap.Cantidad AS FLOAT) as Cantidad,
---	ifnull((SELECT PrecioCompra FROM Compras c JOIN ComprasProductos cp ON c.Id = cp.CompraId WHERE cp.ProductoId = p.Id AND datetime(c.FechaCreado) <= datetime(a.FechaAjuste) ORDER BY datetime(c.FechaCreado) DESC LIMIT 1),p.PrecioCompra) as PrecioCompra,
---	ifnull((SELECT PrecioVenta FROM PreciosProductos pp WHERE p.Id = pp.ProductoId AND datetime(pp.FechaCreado) <= datetime(a.FechaAjuste) ORDER BY datetime(pp.FechaCreado) DESC LIMIT 1),p.PrecioVenta) as PrecioVenta    
---FROM 
---	AjustesProductos ap JOIN Ajustes a ON a.Id = ap.AjusteId JOIN v_productos p on ap.ProductoId = p.id
---WHERE a.TipoAjuste = 0 
---ORDER BY Date(a.FechaAjuste), a.Id
+-- PRUEBAS
+-- 
+-- SELECT 
+-- 	FechaAjuste,
+-- 	SUM(UltimoPrecioCompra * Cantidad) as Inversion,
+-- 	SUM(UltimoPrecioVenta * Cantidad) as Venta,	
+-- 	SUM(UltimoPrecioVenta * Cantidad) - SUM(UltimoPrecioCompra * Cantidad) as Ganancia
+-- FROM v_ventas_productos
+-- GROUP BY date(FechaAjuste)
+-- ORDER BY FechaAjuste;
+-- 
+-- --                      inver   vta     vta re  ganancia
+-- --2022-06-06 14:07:57	55.16	89.0	119.0	33.84
+-- SELECT 
+-- 	FechaAjuste,Pago,Cambio,Cantidad,UltimoPrecioCompra,UltimoPrecioVenta 
+-- FROM 
+-- 	v_ventas_productos
+-- WHERE 
+-- 	date(FechaAjuste) = date('2022-05-30')
+--  ORDER BY FechaAjuste 
+-- 
+
 	
 COMMIT;
 
