692d785f33cecc8dd1476c4492d7ececdc3aefaf
rockdrigo <correo.rodrigo@gmail.com>
2022-07-08 10:18:56 -0500

    downloading from git

--- dbscripts/reports-recreate.sh
+++ dbscripts/reports-recreate.sh
@@ -4,20 +4,16 @@ DBFILE="inventario.db"
 SCRIPTFILE="inventario.sql"
 PROJ_DIR="Ro.Inventario.Web"
 DEV_DB_DIR="Db-dev"
-DB_DIR="Db"
+DB_URL="https://github.com/rogithub/inventario/raw/main/Ro.Inventario.Web/Db/inventario.db"
 
 
-if [ ! -f "../$PROJ_DIR/$DB_DIR/$DBFILE" ]; then
-    echo "../$PROJ_DIR/$DB_DIR/$DBFILE does not exist."
-    exit 1
-fi
 
 echo "creating path if not exists ../$PROJ_DIR/$DEV_DB_DIR"
 if [ ! -d "../$PROJ_DIR/$DEV_DB_DIR" ]; then
     mkdir "../$PROJ_DIR/$DEV_DB_DIR"
 fi
 
-echo "copiying ../$PROJ_DIR/$DB_DIR/$DBFILE to ../$PROJ_DIR/$DEV_DB_DIR/$DBFILE"
-cp "../$PROJ_DIR/$DB_DIR/$DBFILE" "../$PROJ_DIR/$DEV_DB_DIR/$DBFILE"
+echo "getting lates db from $DB_URL"
+curl -O "../$PROJ_DIR/$DEV_DB_DIR/$DBFILE" "$DB_URL"
 echo "executing ./reportes.sql"
 sqlite3 "../$PROJ_DIR/$DEV_DB_DIR/$DBFILE" ".read ./reportes.sql"
