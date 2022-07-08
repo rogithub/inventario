4ffb5eb7062f56d6abe6b3a6396413c75aaee225
rockdrigo <correo.rodrigo@gmail.com>
2022-07-08 09:42:41 -0500

    adding script

--- dbscripts/reports-recreate.sh
+++ dbscripts/reports-recreate.sh
@@ -12,6 +12,11 @@ if [ ! -f "../$PROJ_DIR/$DB_DIR/$DBFILE" ]; then
     exit 1
 fi
 
+echo "creating path if not exists ../$PROJ_DIR/$DEV_DB_DIR"
+if [ ! -d "../$PROJ_DIR/$DEV_DB_DIR" ]; then
+    mkdir "../$PROJ_DIR/$DEV_DB_DIR"
+fi
+
 echo "copiying ../$PROJ_DIR/$DB_DIR/$DBFILE to ../$PROJ_DIR/$DEV_DB_DIR/$DBFILE"
 cp "../$PROJ_DIR/$DB_DIR/$DBFILE" "../$PROJ_DIR/$DEV_DB_DIR/$DBFILE"
 echo "executing ./reportes.sql"
