7a22963e239a1b9e6cb4c53119d4f1cdf71cf654
rockdrigo <correo.rodrigo@gmail.com>
2022-07-22 13:53:03 -0500

    settings repo

--- Ro.Inventario.Web.Tests/Repos/SettinsRepoTests.cs
+++ Ro.Inventario.Web.Tests/Repos/SettinsRepoTests.cs
@@ -3,17 +3,29 @@ using Ro.Inventario.Web.Repos;
 using Ro.Inventario.Web.Entities;
 using Ro.SQLite.Data;
 using Xunit;
+using System.Data.Common;
+using System.Data;
 
 namespace Ro.Inventario.Web.Tests;
-// https://github.com/xunit/assert.xunit
+
 public class SettinsRepoTests
 {
     [Fact]
     public async Task GetValue()
     {
-        var mock = new Mock<IDbAsync>();
+        var s = new Setting()
+        {
+            Key = "IVA",
+            Value = "0"
+        };        
 
-        var sut = new SettingsRepo(mock.Object);
+        var mDb = new Mock<IDbAsync>();
+        mDb.Setup(x => x.GetOneRow(
+            It.IsAny<DbCommand>(),
+            It.IsAny<Func<IDataReader, Setting>>()
+        )).Returns(()=> Task.FromResult(s));
+
+        var sut = new SettingsRepo(mDb.Object);        
 
         var actual = await sut.GetValue("IVA", (value) => {
             return decimal.Parse(value);
